90749b31f404c06e12f9f03426860d8a01b52ee6
SeongYoon <dlatjddbs9844@yonsei.ac.kr>
2026-01-25 13:27:28 +0900

    feat: add repository retrieve logic

---
+++ src/main/java/com/writingboard/server/domain/meeting/repository/RoomParticipantRepository.java
@@ -0,0 +1,29 @@
+package com.writingboard.server.domain.meeting.repository;
+
+import com.writingboard.server.domain.meeting.entity.RoomParticipant;
+import com.writingboard.server.domain.meeting.entity.enums.ParticipantState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomParticipantRepository extends JpaRepository<RoomParticipant, Long> {
+
+    Optional<RoomParticipant> findByRoomIdAndMemberId(Long roomId, Long memberId);
+
+    List<RoomParticipant> findByRoomIdAndState(Long roomId, ParticipantState state);
+
+    List<RoomParticipant> findByRoomId(Long roomId);
+
+    @Query("SELECT rp FROM RoomParticipant rp " +
+           "JOIN FETCH rp.room r " +
+           "WHERE rp.member.id = :memberId " +
+           "AND rp.state = :state " +
+           "AND r.status = 'OPEN'")
+    List<RoomParticipant> findMyActiveRooms(@Param("memberId") Long memberId,
+                                             @Param("state") ParticipantState state);
+
+    boolean existsByRoomIdAndMemberIdAndState(Long roomId, Long memberId, ParticipantState state);
+}
