164f18000635245cc2b0c0d30105f4141b8b0553
2022148084 <cheesetime@yonsei.ac.kr>
2026-02-08 17:36:40 +0900

    Update Swagger endpoint for deployment

--- src/main/java/com/writingboard/server/global/config/OpenApiConfig.java
+++ src/main/java/com/writingboard/server/global/config/OpenApiConfig.java
@@ -5,17 +5,29 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server prodServer = new Server();
+        prodServer.setUrl("https://hsh-server.com");
+        prodServer.setDescription("운영 서버 (HTTPS)");
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("로컬 서버 (HTTP)");
+
         return new OpenAPI()
+                .servers(List.of(prodServer, localServer)) // 서버 리스트 추가
                 .info(new Info()
-                        .title("PLP Server API")
+                        .title("Writing Board Server API")
                         .version("1.0")
                         .description("회의실 및 화이트보드 서비스 API"))
                 .components(new Components()
